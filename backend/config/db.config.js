7acc80a26f71a3462cdde814fc40531ffa74bddd
wafa meftahi <wafa_meftahi@live.fr>
2022-02-05 02:16:08 +0100

    maj fichier config

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 const sequelize = new Sequelize('groupomania','root','wafa.mango2000', {
     host: 'localhost',
     dialect: 'mysql',
-    pool: { //configuration de pool utilisée pour regrouper les connexions à la base de données
+    pool: {
       max: 5,
       min: 0,
       acquire: 30000,
