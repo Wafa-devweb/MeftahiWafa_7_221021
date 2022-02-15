5ed81c4f28491a9e57adf99d027c73a276a1c9fe
wafa meftahi <wafa_meftahi@live.fr>
2022-02-15 17:32:51 +0100

    devfron

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -1,24 +1,25 @@
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('groupomania','root','wafa.mango2000', {
-    host: 'localhost',
-    dialect: 'mysql',
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-    }
-  });
-  try {
-    sequelize.authenticate();
-    console.log('Connection has been established successfully.');
-  } catch (error) {
-    console.error('Unable to connect to the database:', error);
+const sequelize = new Sequelize('groupomania','root','wafa.mango2000',{
+  host: 'localhost',
+  dialect: 'mysql',
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
   }
-  const db = {};
+});
+try {
+  sequelize.authenticate();
+  console.log('Connection has been established successfully.');
+} catch (error) {
+  console.error('Unable to connect to the database:', error);
+}
+  
+const db = {};
 
-  db.Sequelize = Sequelize;
-  db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
 
 
 module.exports = db;
--- backend/models/index.js
+++ backend/models/index.js
@@ -5,7 +5,6 @@ const Sequelize = require("sequelize");
 const sequelize = new Sequelize(process.env.SQL_DATABASE_NAME,process.env.SQL_USER,process.env.SQL_PASSWORD, {
   host: 'localhost',
   dialect: 'mysql',
-  logging: false,
   pool: {
     max: 5,
     min: 0,
@@ -13,9 +12,14 @@ const sequelize = new Sequelize(process.env.SQL_DATABASE_NAME,process.env.SQL_US
     idle: 10000
   }
 });
+try {
+  sequelize.authenticate();
+  console.log('Connection has been established successfully.');
+} catch (error) {
+  console.error('Unable to connect to the database:', error);
+}
 
 const db = {};
-
 
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
